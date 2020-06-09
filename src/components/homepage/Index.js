ac7dc33c113fbeeee396b41f4a86ac0a7e662913
Dave Regg <dave.r.regg@gmail.com>
2020-06-09 17:29:48 -0400

    Test fetching data

--- src/components/homepage/Index.js
+++ src/components/homepage/Index.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 // import PropTypes from 'prop-types';
 
 const Index = () => {
+  const [ paintings, updatePaintings ] = useState([])
+  useEffect(() => {
+    async function fetchData() {
+      const res = await axios.get(`https://www.rijksmuseum.nl/api/en/collection?key=nxwxKjwi&involvedMaker=Rembrandt+van+Rijn`);
+      if (res.data.artObjects) {
+        updatePaintings([...res.data.artObjects])
+      }
+    }
+    fetchData();
+  }, []);
+  if (paintings) {
+    console.log(paintings);
+  }
+  const render = paintings.length === 0 ? (
+    'Still loading'
+  ) : (
+    paintings.map(obj => {
+      return (
+        <div key={obj.id}>
+          <h1>{obj.title}</h1>
+          <img src={obj.webImage.url} alt="" style={{
+            width: '50vw'
+          }} />
+        </div>
+      )
+    })
+  )
   return (
     <div>
       <h1>Welcome to the Homepage</h1>
+      <div>
+        { render }
+      </div>
     </div>
   )
 }
