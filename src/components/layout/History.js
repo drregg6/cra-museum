d065edf2ec180c68332e2ec26734ee940956ca75
Dave Regg <dave.r.regg@gmail.com>
2020-06-12 14:34:01 -0400

    Implemented buttons for History Component to expand / retract full history and delete history

--- src/components/layout/History.js
+++ src/components/layout/History.js
@@ -1,24 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import styles from './history.module.scss';
+import utilStyles from '../../styles/utils.module.scss';
 
 import { connect } from 'react-redux';
+import { deleteHistory } from '../../actions/history';
 
 export const History = ({
+  deleteHistory,
   history: { history, isLoading }
 }) => {
-  const render = isLoading ? ('') : (
+  const [ expand, toggleExpand ] = useState(false);
+  const render = isLoading ? ('') : expand ? (
     history.map(painter => {
-      return <li>{painter}</li>
+      return <li className={styles.item}>{painter}</li>
+    })
+  ) : (
+    history.slice(0,5).map(painter => {
+      return <li className={styles.item}>{painter}</li>
     })
   )
   return (
-    <ul>
+    <ul className={styles.list}>
       { render }
+      {
+        history.length > 5 && (
+          <button
+            className={`${utilStyles.button}`}
+            onClick={() => toggleExpand(!expand)}
+          >
+            { expand ? 'Less' : 'More' }
+          </button>
+        )
+      }
+      {
+        history.length > 0 && (
+          <button
+            className={`${utilStyles.button}`}
+            onClick={() => deleteHistory()}
+          >
+            Delete History
+          </button>
+        )
+      }
     </ul>
   )
 }
 
 History.propTypes = {
+  deleteHistory: PropTypes.func.isRequired,
   history: PropTypes.object
 }
 
@@ -28,5 +58,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  null
+  { deleteHistory }
 )(History);
