399421ee81ef054b142991db8c12eac2f34cf029
Dave Regg <dave.r.regg@gmail.com>
2020-06-14 15:54:47 -0400

    Some more adjustments

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,8 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-        Copyright &copy;{new Date().getFullYear()} <a href="http://www.daveregg.com" target="_blank" rel="noopener noreferrer">Dave Regg</a>
+        Copyright &copy;{new Date().getFullYear()} <a href="http://www.daveregg.com" target="_blank" rel="noopener noreferrer">Dave Regg</a><br />
+        Powered by <a href="https://www.rijksmuseum.nl/" target="_blank" rel="noopener noreferrer">Rijksmuseum</a>
       </p>
     </footer>
   )
