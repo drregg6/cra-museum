3627cc25cbe8d20eeeb16d1ddd15441d7ccbac2d
Dave Regg <dave.r.regg@gmail.com>
2020-06-14 15:33:39 -0400

    Deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, `build`)));
@@ -9,4 +9,4 @@ app.get(`*`, (res, res) => {
   res.sendFile(path.join(__dirname, `build`, `index.html`));
 });
 
-app.listen(PORT, () => console.log('Server running'));
+app.listen(port, () => console.log('Server running'));
