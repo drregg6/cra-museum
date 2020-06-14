a8dc1a218379eb35a4c27a839e85a29ae365ea3f
Dave Regg <dave.r.regg@gmail.com>
2020-06-14 15:17:11 -0400

    npm build ran

---
+++ build/precache-manifest.8ada207e93180965b3ce4e91ab44333c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee341a4e88f1a3d45453ad16c427c8d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6c3bda7d7db1e25abbf",
+    "url": "/static/css/main.c54a8b67.chunk.css"
+  },
+  {
+    "revision": "3f2e40154c6bfaa75a3b",
+    "url": "/static/js/2.61ac1a36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.61ac1a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c3bda7d7db1e25abbf",
+    "url": "/static/js/main.fefc3761.chunk.js"
+  },
+  {
+    "revision": "077eea06179236aa77c0",
+    "url": "/static/js/runtime-main.fa4dbbcb.js"
+  }
+]);
